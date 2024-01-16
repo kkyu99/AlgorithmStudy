8b1473c228f1c3dc317d0dfee7d5f5442fe2b540
KimHyunJin <68643347+khj745700@users.noreply.github.com>
2024-01-17 01:09:46 +0900

    Update 김현진.java

--- w1/조합(2407)/김현진.java
+++ w1/조합(2407)/김현진.java
@@ -1,52 +1,35 @@
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int depth;
-
-    static int[] arr;
-    static boolean[] visited;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        depth = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
-        visited = new boolean[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-        backTracking(0, new ArrayList<>());
-        bw.flush();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        System.out.println(combination(N,M));
     }
 
 
-    static void backTracking(int depth, List<Integer> list) throws IOException {
-        if(depth == Main.depth) {
-            for(int i = 0; i < list.size(); i++) {
-                bw.append(list.get(i).toString()).append(" ");
-            }
-            bw.newLine();
-            return;
+    static BigDecimal combination(int n, int r) {
+        r = Math.min(r, n-r);
+        BigDecimal answer = BigDecimal.valueOf(1);
+        for(int i = 0; i < r; i++) {
+            answer = answer.multiply(BigDecimal.valueOf(n - i));
         }
 
-        for(int i = 0; i < N; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                list.add(arr[i]);
-                backTracking(depth + 1, list);
-                list.remove(list.size() - 1);
-                visited[i] = false;
-            }
+        for(int i = 1; i <= r; i++) {
+            answer = answer.divide(BigDecimal.valueOf(i));
         }
+
+        return answer;
     }
 }
